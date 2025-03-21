a4f6a05278f206fac3646a1d621f8724fa82ed4d
Darchek <marti.busque@gmail.com>
2025-03-21 11:29:56 +0100

    comming soon

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -3,7 +3,17 @@ export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	darkMode: 'class',
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				'bounce-rotate': {
+					'0%, 100%': { transform: 'translateY(0) rotate(0deg)' },
+					'50%': { transform: 'translateY(-25%) rotate(15deg)' }
+				}
+			},
+			animation: {
+				'bounce-rotate': 'bounce-rotate 2s infinite'
+			}
+		},
 	},
 	plugins: [],
 }
