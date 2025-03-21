366af37a496ec0d8b36f63c08735d79d97ba3973
Darchek <marti.busque@gmail.com>
2025-03-21 11:37:24 +0100

    add vercel

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,18 @@ import tailwind from "@astrojs/tailwind"
 
 import robotsTxt from "astro-robots-txt"
 
+import vercel from "@astrojs/vercel";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), robotsTxt()],
   site: 'https://martibusquets.cat/',
   output: 'server',
+
   i18n: {
     locales: ["ca", "en", "es"],
     defaultLocale: "en",
-  }
+  },
+
+  adapter: vercel()
 })
